358e8333c5446dcda859e25ae1e5184d4c0a6c1e
Varvill <jamescook1999@gmail.com>
2020-02-17 11:16:08 +0000

    made game stuff

---
+++ src/com/tutorial/main/Player.java
@@ -0,0 +1,22 @@
+package com.tutorial.main;
+
+import java.awt.*;
+
+public class Player extends GameObject{
+
+
+
+    public Player(int x, int y, ID id){
+        super(x, y, id);
+    }
+
+    public void tick(){
+
+    }
+
+    public void render(Graphics g){
+        g.setColor(Color.white);
+        g.fillRect(x,y,32,32);
+    }
+
+}
